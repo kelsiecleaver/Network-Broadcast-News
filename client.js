3c181f348ef3fe0b03997e2cf37df49ed404cc44
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-01 19:22:00 -1000

    finally finished

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 /*jshint esversion: 6 */
+
+const net = require('net');
+
+const client = net.connect(6969, '0.0.0.0', () => {
+  console.log('connected to server');
+
+client.on('data',(chunk) =>{
+  console.log(chunk.toString());
+});
+process.stdin.on('data', (chunk) => {
+
+    client.write(chunk);
+
+});
+});
+
+
