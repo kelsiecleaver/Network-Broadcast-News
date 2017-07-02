77a3ee4e787c07138b711d9d90d928de6c67c538
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-01 19:22:15 -1000

    server

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
 /*jshint esversion: 6 */
+const net = require('net');
+
+let users = [];
+
+const server = net.createServer((request) => {
+  users.push(request);
+
+request.on('data', (chunk) => {
+  process.stdout.write(chunk);
+  users.forEach((client) =>{
+    if(client === request) return;
+    client.write(chunk);
+
+  });
+
+});
+process.stdin.on('data', (command)=>{
+  request.write(command);
+});
+
+
+});
+server.listen(6969, '0.0.0.0.',() => {
+  console.log('opened server on address');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
