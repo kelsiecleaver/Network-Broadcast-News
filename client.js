692e4ef699d8531e0fa2f55b551711cfb79b175a
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-01 19:27:32 -1000

    code cleanup

--- client.js
+++ client.js
@@ -4,15 +4,12 @@ const net = require('net');
 
 const client = net.connect(6969, '0.0.0.0', () => {
   console.log('connected to server');
-
-client.on('data',(chunk) =>{
+  client.on('data',(chunk) =>{
   console.log(chunk.toString());
 });
 process.stdin.on('data', (chunk) => {
-
-    client.write(chunk);
-
-});
+  client.write(chunk);
+  });
 });
 
 
